fe2301c7a6dd0f92beef45cf9e3ba2cbb9ce1fde
laurakopunovic <77838909+laurie2412@users.noreply.github.com>
2021-01-30 16:49:52 +0100

    Added form submition function

--- src/app.js
+++ src/app.js
@@ -28,4 +28,20 @@ function formatHours(timestamp) {
   return `${hours}:${minutes}`;
 }
 
+function searchCity(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+}
 
+function changeCity(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  searchCity(cityInputElement.value);
+
+let searchForm = document.querySelector("#city-search-form");
+searchForm.addEventListener("submit", changeCity);
+
+searchCity("Ljubljana");
