59fcfa7267b9ce9fd14cf88482d57ce095764d3e
laurakopunovic <77838909+laurie2412@users.noreply.github.com>
2021-01-31 10:39:01 +0100

    Implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -94,6 +94,23 @@ function changeCity(event) {
   searchCity(cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
 let searchForm = document.querySelector("#city-search-form");
 searchForm.addEventListener("submit", changeCity);
 
